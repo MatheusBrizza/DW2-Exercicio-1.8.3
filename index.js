274ac1e9e92c8bb9ce063ca1d848ddcd9280b99e
MatheusBrizza <matheusbj95@gmail.com>
2022-08-14 00:42:57 -0300

    Exercício melhorado com ajuda do professor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // run `node index.js` in the terminal
-
+/*
 function url(prot, dom, pat1, pat2, pat3) {
   const protocolo = prot + '://';
 
@@ -27,3 +27,21 @@ console.log(
     'rs+porto-alegre+cidade-baixa'
   )
 );
+*/
+
+function gerarURL(protocolo, dominio, options) {
+  const { paths, queries } = options;
+  const pathString = paths.join('/').replace(/\s/g, '+');
+  const queriesString = new URLSearchParams(queries).toString();
+  return `${protocolo}//${dominio}/${pathString}?${queriesString}`;
+}
+
+const url = gerarURL('https', 'www.auxiliadorapredial.com.br', {
+  paths: ['alugar', 'residencial', 'rs porto-alegre cidade-baixa'],
+  queries: {
+    quartos: 2,
+    'sem-fiador-ou-garantias': true,
+  },
+});
+
+console.log(url);
